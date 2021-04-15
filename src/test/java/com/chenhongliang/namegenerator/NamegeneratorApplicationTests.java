17894d3d927dcccc56093e8a50f8717390ad0a8f
wade <wadechen@outlook.com>
2021-04-15 17:32:22 +0800

    first commit

---
+++ src/test/java/com/chenhongliang/namegenerator/NamegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenhongliang.namegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamegeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
