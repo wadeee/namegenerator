843d59c0a2ee2dc59c700b5142b0b0e6be9a96c0
wade <wadechen@outlook.com>
2021-04-20 18:59:35 +0800

    single character manage

---
+++ inst.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+## build the jar ##
+gradle bootJar
+
+## stop mysqld ##
+#systemctl stop mysqld
+## add namegenerator to service ##
+echo y | cp ./namegenerator.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable namegenerator
+#systemctl start namegenerator
+systemctl restart namegenerator
+
+## add config to nginx ##
+echo y | cp ./namegenerator.nginx.http.conf /etc/nginx/conf.d/
+systemctl restart nginx ## please make sure nginx installed ##
+
+## start mysqld ##
+#systemctl start mysqld
